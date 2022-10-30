33b38843027e3284410cc63db831651f50317790
Marshall Buck <dev@marshallbuck.com>
2022-10-30 11:24:57 -0500

    added bucketeer

--- config.js
+++ config.js
@@ -11,8 +11,8 @@ const PORT = +process.env.PORT || 3001;
 // Use dev database, testing database, or via env var, production database
 function getDatabaseUri() {
   return (process.env.NODE_ENV === "test")
-      ? "sharebnb_test"
-      : process.env.DATABASE_URL || "sharebnb";
+    ? "sharebnb_test"
+    : process.env.DATABASE_URL || "sharebnb";
 }
 
 // Speed up bcrypt during tests, since the algorithm safety isn't being tested
@@ -20,19 +20,25 @@ function getDatabaseUri() {
 // WJB: Evaluate in 2021 if this should be increased to 13 for non-test use
 const BCRYPT_WORK_FACTOR = process.env.NODE_ENV === "test" ? 1 : 12;
 
-// AWS S3 setup
-const REGION = process.env.REGION; //e.g. "us-east-1"
-const BUCKET_NAME = process.env.BUCKET_NAME;
-const ACCESS_KEY_ID= process.env.ACCESS_KEY_ID;
-const SECRET_ACCESS_KEY= process.env.SECRET_ACCESS_KEY;
+
+
+// // AWS S3 setup
+const BUCKETEER_AWS_REGION = process.env.BUCKETEER_AWS_REGION;
+const BUCKETEER_BUCKET_NAME = process.env.BUCKETEER_BUCKET_NAME;
+const BUCKETEER_AWS_ACCESS_KEY_ID = process.env.BUCKETEER_AWS_ACCESS_KEY_ID;
+const BUCKETEER_AWS_SECRET_ACCESS_KEY = process.env.BUCKETEER_AWS_SECRET_ACCESS_KEY;
+
+
+
+
 
 module.exports = {
   SECRET_KEY,
   PORT,
   BCRYPT_WORK_FACTOR,
   getDatabaseUri,
-  REGION,
-  BUCKET_NAME,
-  ACCESS_KEY_ID,
-  SECRET_ACCESS_KEY,
+  BUCKETEER_AWS_ACCESS_KEY_ID,
+  BUCKETEER_AWS_REGION,
+  BUCKETEER_BUCKET_NAME,
+  BUCKETEER_AWS_SECRET_ACCESS_KEY
 };
